b275703626aa861b27c243a6d1029fb705438d3d
RolandWarburton <warburtonroland@gmail.com>
2020-09-03 12:19:12 +1000

    :heavy_plus_sign: Add IP debug to sso_server

--- sso-server/app/index.js
+++ sso-server/app/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const ip = require("internal-ip");
 require("dotenv").config();
-const PORT = 3010;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
@@ -9,7 +9,7 @@ const port = process.env.DB_PORT;
 const database = process.env.DB_DATABASE;
 const auth = process.env.DB_AUTHENTICATION_DATABASE;
 const userAuth = `${username}:${password}`;
-const url = `mongodb://${userAuth}@10.10.10.12:${port}/${database}?authsource=${auth}`;
+const url = `mongodb://${userAuth}@sso_database:${port}/${database}?authsource=${auth}`;
 console.log(url);
 // mongodb://admin:rhinos@devel:27018/sso?authSource=sso
 
@@ -28,6 +28,6 @@ mongoose.connect(
 	}
 );
 
-app.listen(PORT, () => {
-	console.info(`sso-server listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+	console.info(`sso-server listening on ${ip.v4.sync()}:${process.env.PORT}`);
 });
